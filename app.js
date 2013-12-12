57cb2933cc303cb511c53c1f7f1dd5a42a29c61b
unknown <graefr@MSOE-00026491.msoe.edu>
2013-12-12 16:27:15 -0600

    Game Framework Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight - 50;
+var gameHeight = 10000;
+var paddingBottom = 100;
+
+var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'StarChaser', {preload: preload, create: create, update: update, render: render});
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
+
+function render(){
+
+}
